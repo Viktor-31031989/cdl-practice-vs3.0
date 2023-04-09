c414dd5b96811d78894bd70646a8c0f8f04913ae
viktor <viktordikovich@gmail.com>
2023-04-08 23:39:49 -0500

    bh

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,7 +11,7 @@ function Header() {
             </h1>
             <div className='header-home-signIn'>
                 <Home />
-                <SignIn/>
+                <SignIn />
             </div>
         </div>
     );
