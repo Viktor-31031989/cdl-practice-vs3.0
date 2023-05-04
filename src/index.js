fa9968adc1abeabeb11c1e05ce5e05c7101d2a53
viktor <viktordikovich@gmail.com>
2023-05-04 15:48:08 -0500

    fixed

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import 'bootstrap/dist/js/bootstrap.min.js';
 import { Provider } from "react-redux";
 import { store } from "./store/store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot( document.getElementById('root') );
 root.render(
-    <Provider store={store}>
+    <Provider store={ store }>
         <BrowserRouter>
             <App />
         </BrowserRouter>
